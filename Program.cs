3ab884d811660d02086cf285baf94c15be84688c
LAB53 <lab53@fatec.com>
2025-03-17 10:25:29 -0300

    Substitui as letras 'r e R' por 'l e L'

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string frase;
+string dislalia;
+
+Console.Clear();
+
+Console.WriteLine("Digite uma frase: ");
+frase = Console.ReadLine()!;
+
+Console.Clear();
+
+dislalia = frase
+                .Replace("r", "l")
+                .Replace("R", "L");
+
+Console.WriteLine($"Você disse: {frase}");
+Console.WriteLine($"O Cebolinha diria: {dislalia}");
